2ec357347d2095411e67b23c8a0407135df4ee9e
Pierre Roos <101295194+PierreRoos@users.noreply.github.com>
2022-03-31 09:28:46 -0700

    update to headless mode

--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -9,8 +9,19 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select  # add this import for drop down list
 
-s = Service(executable_path='../chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
 
 
 def setUp():
